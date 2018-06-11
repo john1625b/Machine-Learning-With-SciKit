9325c879f5024f42d88c0c471aa74898a9d8812a
John <john1625b@gmail.com>
2018-06-11 04:24:04 -0400

    adds cutom classifier

--- apples-oranges.py
+++ apples-oranges.py
@@ -4,4 +4,7 @@ features = [[140, 1], [130, 1], [150, 0], [170, 0]] # 1 smooth, 0 bumpy
 labels = [0, 0, 1, 1] # 0 apples, 1 orange
 
 clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print( clf.predict([[150, 0]]) )
 
